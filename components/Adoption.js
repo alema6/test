70fa8102123fa1f696082fe3f661672bffe8789d
aminahodzic <aminahodzic600@gmail.com>
2025-06-18 12:42:28 +0200

    frotend

--- components/Adoption.js
+++ components/Adoption.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import Image from 'next/image';
+import Img1 from '../public/img/adoption/img1.png';
+
 
 const Adoption = () => {
-  return <div>Adoption</div>;
+  return (
+  <section className='bg-adoption bg-cover bg-center bg-no-repeat min-h-[760px] py-8 flex'>
+    <div className='container mx-auto flex flex-col lg:flex-row items-center justify-center pag-x-16'>
+      <div className='flex-1 mb-6 lg:mb-0'>
+        <Image src={Img1} width={542} height={560} alt=''/>
+      </div>
+      <div className='flex-1 text-cream text-center max-w-md lg:text-left lg:max-w-none'>
+        <h2 className='h2 mb-6'>Making pet parenting easy for everyone</h2>
+        <p className='mb-6'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <ul className='mb-[38px] flex flex-col gap-y-4 lg:list-disc lg:pl-4'>
+          <li>Adoption</li>
+          <li>Frozen Raw</li>
+          <li>Next Day Delivery</li>
+        </ul>
+        <button className='btn btn-primary mx-auto lg:mx-0'>Explore</button>
+
+      </div>
+    </div>
+  </section>
+
+  );
 };
 
 export default Adoption;
--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,7 +1,62 @@
 import React from 'react';
+import Image from 'next/image';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import{Navigation} from 'swiper';
+import 'swiper/css/navigation';
+import 'swiper/css';
+import Service1Icon from '../public/img/services/service-icon1.svg';
+import Service2Icon from '../public/img/services/service-icon2.svg';
+import Service3Icon from '../public/img/services/service-icon3.svg';
+
+const services=[
+  {
+    image: Service1Icon,
+    name:'Pharmacy',
+    description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    btnText:'Explore'
+  },
+
+  {
+    image: Service2Icon,
+    name:'Breed-specific Haircuts',
+    description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    btnText:'Explore'
+  },
+  {
+    image: Service3Icon,
+    name:'Cloths',
+    description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    btnText:'Explore'
+  
+  }
+];
+
 
 const ServiceSlider = () => {
-  return <div>ServiceSlider</div>;
+  return (
+  <Swiper slidesPerView={1} spaceBetween={30} navigation={true} modules={[Navigation]} breakpoints={{
+    768:{
+      slidesPerView:2,
+    },
+  }}
+  className='serviceSlider min-h-[680px]'
+  
+  >
+    {services.map((service, index)=>{
+      return(
+      <SwiperSlide key={index} className='border border-primary/20 bg-cream min-h-[560px] rounded-[66px] py-16 px-8'>
+      <Image className='mb-9' src={service.image} alt={service.name} />
+      <div className='text-[26px] font-medium mb-4'>{service.name}</div>
+      <div className='text-[20px] mb-8'>{service.description}</div>
+      <button className='btn btn-primary'>{service.btnText}</button>
+      </SwiperSlide>
+
+      );
+    })}
+  </Swiper>);
 };
 
 export default ServiceSlider;
